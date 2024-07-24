4291a0c66547fdc154692ea74c3f65c9a655e030
Vivek Dutt Sharma <158206172+vivekdsharma@users.noreply.github.com>
2024-07-24 17:03:12 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,69 @@
+const body = document.body
+const refresh= document.getElementById('refresh')
+const submit= document.getElementById('submit')
+const input = document.getElementById('tobeentered').value
+
+const container= document.getElementById('container')
+const char = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+let captcha=""
+
+const heading = document.getElementById("heading")
+
+body.onload = function generateCaptcha(){
+
+    const clength = 6 ;
+
+    for (let i = 0; i < clength; i++) {
+       const randomIndex=Math.floor(Math.random()*char.length)
+       captcha += char.substring(randomIndex,randomIndex+1)
+        
+    }
+
+    document.getElementById('_show').value=captcha
+
+    heading.innerText="Captcha Generator"
+}
+
+
+submit.onclick= function CheckInfo(){
+
+    
+    if (input === "") {
+        heading.innerText="Please Enter The Captcha First"
+        container.style="border-color:yellow"
+
+    }
+
+    else if( input === captcha){
+        heading.innerText="Congratulations"
+        container.style="border-color:green"
+    }
+
+    else{
+        heading.innerText="Try Again"
+        container.style="border-color:red"
+}
+}
+
+
+
+refresh.onclick= function refreshCaptcha(){
+    
+    const clength = 6 ;
+    let Newcaptcha =""
+
+    for (let i = 0; i < clength; i++) {
+       const randomChar= char [Math.floor(Math.random()*char.length)]
+       Newcaptcha += randomChar
+        
+    }
+    captcha=Newcaptcha
+
+    document.getElementById('_show').value=captcha
+
+    input.value = "";
+
+}
+
+
